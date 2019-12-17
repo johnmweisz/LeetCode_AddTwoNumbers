dc970ef277c92d8f5ebc1cd2a6bcd0ed92bfcf37
John Weisz <johnmweisz@gmail.com>
2019-12-17 15:26:11 -0800

    Add project files.

---
+++ LeetCode_AddTwoNumbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var r1 = l1;
+            var r2 = l2;
+            var newList = new ListNode(0);
+            var Head = newList;
+            var carry = 0;
+            while (r1 != null || r2 != null)
+            {
+                var a = r1?.val ?? 0;
+                var b = r2?.val ?? 0;
+                var sum = a + b + carry;
+                carry = sum / 10;
+                newList.next = new ListNode(sum % 10);
+                newList = newList.next;
+                r1 = r1?.next;
+                r2 = r2?.next;
+            }
+
+            if (carry != 0)
+            {
+                newList.next = new ListNode(carry);
+            }
+
+            return Head.next;
+        }
+    }
+}
---
+++ LeetCode_AddTwoNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode_AddTwoNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("LeetCode_AddTwoNumbers")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb811072-fd09-4101-8030-46a49aeb2c79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
